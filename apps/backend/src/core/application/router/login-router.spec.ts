909411c65975a78587cd84e5068e3ab9f742ae6e
lionelp.dev <lionelp.dev@gmail.com>
2022-02-15 22:06:02 +0100

    feat: ensure LoginRouter return 400 if no password is provided

--- apps/backend/src/core/application/router/login-router.spec.ts
+++ apps/backend/src/core/application/router/login-router.spec.ts
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
       };
@@ -13,6 +13,16 @@ describe("Login Router", () => {
     const httpRequest = {
       body: {
         password: "any_password",
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+  it("should return 400 if no password is not provided", () => {
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        password: "any_email@email.com",
       },
     };
     const httpResponse = sut.route(httpRequest);
