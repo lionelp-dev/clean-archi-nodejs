9686c6b93f3d4b44ea0cd8bd7fb024166f912a23
lionelp.dev <lionelp.dev@gmail.com>
2022-02-16 04:54:12 +0100

    feat: ensure LoginRouter return 500 if httpRequest has no body

--- apps/backend/src/core/application/router/login-router.spec.ts
+++ apps/backend/src/core/application/router/login-router.spec.ts
@@ -1,6 +1,6 @@
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
       return {
         statusCode: 500,
       };
@@ -41,6 +41,12 @@ describe("Login Router", () => {
   it("should return 500 if no httpRequest is not provided", () => {
     const sut = MakeSut();
     const httpResponse = sut.route();
+    expect(httpResponse.statusCode).toBe(500);
+  });
+  it("should return 500 if no body in httpRequest is provided", () => {
+    const sut = MakeSut();
+    const httpRequest = {};
+    const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
   });
 });
