fd9627e8df5348f59fada80a0faa19301f22a4f5
lionelp.dev <lionelp.dev@gmail.com>
2022-02-16 04:36:54 +0100

    feat: ensure LoginRouter return 500 if invalid httpRequest is provided

--- apps/backend/src/core/application/router/login-router.spec.ts
+++ apps/backend/src/core/application/router/login-router.spec.ts
@@ -1,5 +1,10 @@
 class LoginRouter {
   route(httpRequest) {
+    if (!httpRequest) {
+      return {
+        statusCode: 500,
+      };
+    }
     if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400,
@@ -27,5 +32,10 @@ describe("Login Router", () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+  });
+  it("should return 500 if no httpRequest is not provided", () => {
+    const sut = new LoginRouter();
+    const httpResponse = sut.route();
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
