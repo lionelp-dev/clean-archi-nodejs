d20369215272bfdb4515a3e10a5ff40c7a4c6b6e
lionelp.dev <lionelp.dev@gmail.com>
2022-02-19 12:20:21 +0100

    refactor: LoginRouter

--- apps/backend/src/core/application/router/login-router.ts
+++ apps/backend/src/core/application/router/login-router.ts
@@ -5,25 +5,21 @@ export class LoginRouter {
     this.authUseCase = AuthUseCase;
   }
   async route(httpRequest?: any): Promise<any> {
-    if (
-      !httpRequest ||
-      !httpRequest.body ||
-      !this.authUseCase ||
-      !this.authUseCase.auth
-    ) {
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return HttpResponse.badRequest("email");
+      }
+      if (!password) {
+        return HttpResponse.badRequest("password");
+      }
+      const accessToken = this.authUseCase.auth(email, password);
+      if (!accessToken) {
+        return HttpResponse.unAuthorized();
+      }
+      return HttpResponse.ok({ accessToken });
+    } catch (error) {
       return HttpResponse.internalServerError();
     }
-    const { email, password } = httpRequest.body;
-    if (!email) {
-      return HttpResponse.badRequest("email");
-    }
-    if (!password) {
-      return HttpResponse.badRequest("password");
-    }
-    const accessToken = this.authUseCase.auth(email, password);
-    if (!accessToken) {
-      return HttpResponse.unAuthorized();
-    }
-    return HttpResponse.ok({ accessToken });
   }
 }
